5b813bfaeef7b9f7f68da0efb5b824bc2593c504
Hakan <hakanonize35@gmail.com>
2022-05-26 13:48:12 +0300

    uncommented rsu part

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,40 +5,40 @@ const NavBar = () => {
   return (
     <header>
       <nav
-        id="navbar"
-        className="navbar-dark navbar navbar-expand-lg navbar-light"
+        id='navbar'
+        className='navbar-dark navbar navbar-expand-lg navbar-light'
       >
-        <div className="container">
+        <div className='container'>
           <Link href={'/'}>
-            <Image src="/Logo.png" width={216} height={68} />
+            <Image src='/Logo.png' width={216} height={68} />
           </Link>
 
           <button
-            aria-controls="navbarCollapse"
-            className="navbar-toggler collapsed"
-            type="button"
-            aria-label="Toggle navigation"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
+            aria-controls='navbarCollapse'
+            className='navbar-toggler collapsed'
+            type='button'
+            aria-label='Toggle navigation'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarCollapse'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
 
-          <div id="navbarCollapse" className="navbar-collapse collapse">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link href="/">
-                  <a className="nav-link">On-Chair Data Infrastructure</a>
+          <div id='navbarCollapse' className='navbar-collapse collapse'>
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item'>
+                <Link href='/'>
+                  <a className='nav-link'>On-Chair Data Infrastructure</a>
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link href="/">
-                  <a className="nav-link">Secure Data Access Layer</a>
+              <li className='nav-item'>
+                <Link href='/'>
+                  <a className='nav-link'>Secure Data Access Layer</a>
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link href="/">
-                  <a className="nav-link">KoZMITO Coin</a>
+              <li className='nav-item'>
+                <Link href='/'>
+                  <a className='nav-link'>KoZMITO Coin</a>
                 </Link>
               </li>
             </ul>
