0349ffb259acad34e803018496712b48a56bc79c
Hakan Onize <hakanonize35@gmail.com>
2022-05-26 18:01:16 +0300

    Completed website

---
+++ components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Footer = () => {
+  return (
+    <div className="row align-items-center">
+      <div className="col-md-5">
+        <Link href={'/'}>
+          <Image src="/Logo.png" width={216} height={68} />
+        </Link>
+      </div>
+
+      <div className="col-md-6">
+        Copyright © 2010-2021 KoZMITO. All rights reserved.
+      </div>
+      <div className="col-md-1 py-md-5">
+        <Link href={'https://twitter.com/soarrobotics'}>
+          <Image
+            src="/twitter.png"
+            width={32}
+            height={27}
+            className="cursor-pointer"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
