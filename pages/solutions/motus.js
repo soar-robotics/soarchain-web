776719e593c07e821c991900c77f56c983682771
Amirhossein <a.khoshbakht79@gmail.com>
2022-07-06 21:01:11 +0300

    Initiate solutions/motus page

---
+++ pages/solutions/motus.js
@@ -0,0 +1,25 @@
+function Motus() {
+  return (
+    <main className='motus'>
+      <section className='showcase'>
+        <div className='container d-flex'>
+          <div className='content'>
+            <span className='text-title'>
+              Mine crypto <br /> with your car
+            </span>
+            <p className='mt-3'>
+              Proof-of-availability is sustained through plug-and-play hardware, which are a set of
+              open-source connectivity modules with V2V, V2N, V2P and V2X capabilities.
+              <br />
+              <br />
+              The system is designed to fit different vehicles and user configurations(4-wheelers,
+              2-wheelers, pedestrians, micromobility, Road-side units).
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Motus;
