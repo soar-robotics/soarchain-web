8c58d30c3f16cb9d582ea980c36e8321a82b54a8
Amirhossein <a.khoshbakht79@gmail.com>
2022-07-06 18:59:22 +0300

    Enhance solutions/soarchain page

--- pages/solutions/soarchain.js
+++ pages/solutions/soarchain.js
@@ -16,18 +16,20 @@ function Soarchain() {
           <div className='showcase-content showcase-content-left'>
             <h5 className='text-purple text-uppercase fw-bold d-none d-md-block'>soarchain is</h5>
             <span className='text-title'>
-              Decentralized connectivity layer that facilitates the real-time interaction and
-              transaction between vehicles
+              Decentralized connectivity layer for real-time transactions between vehicles
             </span>
             <p className='mt-3'>
               We offer a trustless, permissionless, decentralized and distributed way to enable
               connectivity between vehicles. <br />
+              <br />
               Participant vehicles of the network are incentivized to cryptographically prove their
               availability to the other participants through the network Participants prove their
               availability by broadcasting standardized V2X messages through an Cellular V2X
               (LTE-V2X) air interface. <br />
+              <br />
               The broadcast messages are received and verified by other network participants. Each
               participant in the network is expected to both broadcast and receive messages.
+              <br />
               <br />
               Broadcasting honest and correct messages & Receiving and verifying these messages are
               rewarded through the network’s incentive mechanism.
