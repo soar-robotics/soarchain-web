ede8e2d434280b39b28d0cd62d9f24a62ea8b896
Amirhossein <a.khoshbakht79@gmail.com>
2022-07-07 20:23:10 +0300

    Add main features section in solutions/motus page

--- pages/solutions/motus.js
+++ pages/solutions/motus.js
@@ -1,3 +1,5 @@
+import { motusMainFeatures } from '../../utils/context/motusContext';
+
 function Motus() {
   return (
     <main className='motus'>
@@ -15,6 +17,21 @@ function Motus() {
               The system is designed to fit different vehicles and user configurations(4-wheelers,
               2-wheelers, pedestrians, micromobility, Road-side units).
             </p>
+          </div>
+        </div>
+      </section>
+      <section className='features'>
+        <div className='container'>
+          <p className='text-title text-center'>Main features</p>
+          <div className='features-list mt-5'>
+            {motusMainFeatures.map((item, index) => (
+              <span
+                key={index}
+                className={`features-list-item-${index % 2 === 0 ? 'left' : 'right'}`}
+              >
+                {item}
+              </span>
+            ))}
           </div>
         </div>
       </section>
--- utils/context/motusContext.js
+++ utils/context/motusContext.js
@@ -4,3 +4,10 @@ export const motusExploreItems = [
   'Get rewarded with cryptocurrency',
   'Compatible with your existing car',
 ];
+
+export const motusMainFeatures = [
+  'Direct vehicle-to-vehicle wireless communication',
+  'Cellular communication(5G-NR/4G-LTE)',
+  'Storage and processing power for executing proof-of-availability',
+  'Wi-Fi and Bluetooth for user/driver connectivity',
+];
