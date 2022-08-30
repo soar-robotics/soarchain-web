d889746886fed2fa4e018a04e90a044cea46fe44
Amirhossein <a.khoshbakht79@gmail.com>
2022-08-30 14:25:46 +0300

    Add Drip form

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,22 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: ` var _dcq = _dcq || [];
+                        var _dcs = _dcs || {};
+                        _dcs.account = '9032744';
+                      
+                        (function() {
+                          var dc = document.createElement('script');
+                          dc.type = 'text/javascript'; dc.async = true;
+                          dc.src = '//tag.getdrip.com/9032744.js';
+                          var s = document.getElementsByTagName('script')[0];
+                          s.parentNode.insertBefore(dc, s);
+                        })();`,
+            }}
+          />
         </body>
       </Html>
     );
